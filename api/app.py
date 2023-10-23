848caf459b80ff38029f8f4af02fbb55bb4c1dd1
zh3423 <zh3423@ic.ac.uk>
2023-10-23 16:12:45 +0100

    sse_3_6

--- api/app.py
+++ api/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 @app.route("/submit", methods = ["POST"])
 def submit():
