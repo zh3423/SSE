167fd62542e6fc3c1dbcbe656ba3effcafbb9df8
zh3423 <zh3423@ic.ac.uk>
2023-10-25 11:03:56 +0100

    222

--- api/app.py
+++ api/app.py
@@ -9,8 +9,23 @@ def hello_world():
     return render_template("index.html")
 
 
+def process_query(name):
+    if name == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 \
+million years ago"
+
+    if name == "asteroids":
+        return "Unknown"
+
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+
+@app.route("/query", methods=["GET"])
+def handle_query():
+    q_query = request.args.get('q')
+    return process_query(q_query)
