1aa02f0f6a876d50148f833f80d65f089997fd77
zh3423 <zh3423@ic.ac.uk>
2023-10-30 16:04:25 +0000

    maaaaaah

--- api/app.py
+++ api/app.py
@@ -17,6 +17,9 @@ million years ago"
     if name == "asteroids":
         return "Unknown"
 
+    if name == "What is your name?":
+        return "15mahomes"
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -29,14 +32,3 @@ def submit():
 def handle_query():
     q_query = request.args.get('q')
     return process_query(q_query)
-
-
-def process_query_2(query):
-    if query == "What is your name?":
-        return "15mahomes"
-
-
-@app.route("/query", methods=["GET"])
-def query_endpoint():
-    q_query = request.args.get('q')
-    return process_query_2(q_query)
