0b4dbc1d3cee2f4a9b7ad48f2447c9c2b3085e1a
zh3423 <zh3423@ic.ac.uk>
2023-10-30 15:38:16 +0000

    ma

--- api/app_test.py
+++ api/app_test.py
@@ -4,11 +4,9 @@ import unittest
 
 class TestApp(unittest.TestCase):
 
-
 def test_knows_about_dinosaurs():
     assert process_query("dinosaurs") == "Dinosaurs ruled the Earth 200 \
 million years ago"
-
 
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
